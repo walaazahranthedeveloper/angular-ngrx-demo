1fffaf8f246927d2dcf3a03591251e893d8c4e64
walaa_zahran <walaazahran183@gmail.com>
2024-09-23 13:39:15 +0300

    State Management using NgRx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,49 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { decrement, increment, reset } from '../../store/counter/counter.actions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+   
+   <div class="container">
+      <h1>Counter App with NgRx</h1>
+      <h2>{{ count$ | async }}</h2>
 
-    <router-outlet />
+      <button (click)="increment()" class="btn btn-primary">Increment</button>
+      <button (click)="decrement()" class="btn btn-danger">Decrement</button>
+      <button (click)="reset()" class="btn btn-secondary">Reset</button>
+  </div>
+
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'angular-ngrx-demo';
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    // Select the counter state
+    this.count$ = this.store.select('count');
+  }
+
+  // Dispatch the increment action
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  // Dispatch the decrement action
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  // Dispatch the reset action
+  reset() {
+    this.store.dispatch(reset());
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideStore } from '@ngrx/store';
 import { AppComponent } from './app/app.component';
+import { counterReducer } from '../store/counter/counter.reducer';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideStore({ count: counterReducer }), // Provide store directly in the standalone app
+  ]
+}).catch(err => console.error(err));
